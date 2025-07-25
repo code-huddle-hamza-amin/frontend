e527e2ae10dea2e0f2212a4fc82de47c50b655b0
code-huddle-hamza-amin <hamza.amin@code-huddle.com>
2025-07-25 17:49:09 +0500

    minmal_ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,18 +5,68 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="SplashScreen" options={{ headerShown: false }} />
       <Stack.Screen 
-        name="reg-login" 
+        name="screens/reg-login/login" 
         options={{ 
           headerShown: false,
-          gestureEnabled: false // Disable swipe back gesture
+          gestureEnabled: false
         }} 
       />
       <Stack.Screen 
-        name="screens" 
+        name="screens/reg-login/onboarding" 
         options={{ 
           headerShown: false,
-          gestureEnabled: false // Disable swipe back gesture
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/reg-login/signup" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/home/index" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/forgotpassword/index" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/forgotpassword/changepassword" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/forgotpassword/otp" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/recored/index" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
+        }} 
+      />
+      <Stack.Screen 
+        name="screens/Managepermission/index" 
+        options={{ 
+          headerShown: false,
+          gestureEnabled: false
         }} 
       />
     </Stack>
